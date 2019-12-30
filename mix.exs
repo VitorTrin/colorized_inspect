78be08823cc88f090adb045be419d7348329a197
VitorTrin <emailreservadovitor@gmail.com>
2019-12-30 14:11:41 -0300

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ColorizedInspect.MixProject do
   def project do
     [
       app: :colorized_inspect,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.7",
       deps: deps(),
       description: description(),
